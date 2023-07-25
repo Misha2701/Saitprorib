f5453a7127283851b906a3ebb3a8dee425b994f6
Misha2701 <140446170+Misha2701@users.noreply.github.com>
2023-07-25 11:11:25 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["index.html"]); toolbox.router.get("/*", toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
